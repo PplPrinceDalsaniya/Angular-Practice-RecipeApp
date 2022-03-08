99807e513fa1909de90490c4d6b357b7008a31b9
PplPrinceDalsaniya <prince.dalsaniya@playpowerlabs.com>
2022-03-09 02:14:18 +0530

    Started Adding Child Routes
    - Added Child route for page where no recipe is selcted.
    - For that we also created a component recipe-start.
    - removed if-else logic for showing a recipe in recipe detail section.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { AppRoutingModule } from './app-routing.module';
     RecipesDetailComponent,
     RecipeItemComponent,
     ShoppingListComponent,
-    ShoppingEditComponent
+    ShoppingEditComponent,
+    RecipeStartComponent
   ],
   imports: [
     BrowserModule,
