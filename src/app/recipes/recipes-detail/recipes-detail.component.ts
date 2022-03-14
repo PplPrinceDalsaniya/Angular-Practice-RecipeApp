40064a5fe7a99ae7c4884d6feffd200c52360455
PplPrinceDalsaniya <prince.dalsaniya@playpowerlabs.com>
2022-03-14 10:46:55 +0530

    Adding delete and clear functionality
    - from recipedetail page in dropdown menu, delete recipe working.
    - while editing recipe, cancel button will take back to the detail page.
    - if you save the changes in recipe it will take back to recipe detail page.

--- src/app/recipes/recipes-detail/recipes-detail.component.ts
+++ src/app/recipes/recipes-detail/recipes-detail.component.ts
@@ -34,4 +34,8 @@ export class RecipesDetailComponent implements OnInit {
   onEditRecipe() {
     this.router.navigate(['edit'], {relativeTo: this.route});
   }
+
+  onDeleteRecipe() {
+    this.recipeService.deleteRecipe(this.id);
+  }
 }
