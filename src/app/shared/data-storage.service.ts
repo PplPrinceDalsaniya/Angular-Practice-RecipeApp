05ad474907723fc732aa3bca80a448386aa7d442
PplPrinceDalsaniya <prince.dalsaniya@playpowerlabs.com>
2022-04-01 11:21:34 +0530

    Resolving Data before Loading - recipeResolverService   - We have resolve method where we are fetching all recipes. - AppRoutingModule   - adding resolve property.

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { map } from "rxjs";
+import { map, tap } from "rxjs";
 import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
 
@@ -21,15 +21,17 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        this.http
+        return this.http
             .get<Recipe[]>("https://ng-recipebook-5e29c-default-rtdb.firebaseio.com/recipes.json")
-            .pipe(map(recipes => {
-                return recipes.map(recipe => {
-                    return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] }
+            .pipe(
+                map(recipes => {
+                    return recipes.map(recipe => {
+                        return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] }
+                    })
+                }),
+                tap(recipes => {
+                    this.recipesService.setRecipes(recipes);
                 })
-            }))
-            .subscribe(response => {
-                this.recipesService.setRecipes(response);
-            })
+            )
     }
 }
