8e52ae6a0fcc1f537a953d6efddcacc8594fba9e
PplPrinceDalsaniya <prince.dalsaniya@playpowerlabs.com>
2022-04-04 11:09:24 +0530

    Adding logout functionality

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
 import { BehaviorSubject, Subject, throwError } from "rxjs";
 import { catchError, tap } from "rxjs/operators";
 import { User } from "./user.model";
@@ -19,7 +20,10 @@ export interface AuthResponseData {
 export class AuthService {
     user = new BehaviorSubject<User>(null);
 
-    constructor(private http: HttpClient){}
+    constructor(
+        private http: HttpClient,
+        private router: Router
+    ){}
 
     signUp(email: string, password: string) {
         return this.http.post<AuthResponseData>("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCM-6RoM3JzmqC2xqiEGc1jodgP4jRMgMw", 
@@ -82,6 +86,11 @@ export class AuthService {
                 this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn);
             })
         );
+    }
+
+    logout() {
+        this.user.next(null);
+        this.router.navigate(['/auth']);
     }
 
     private handleAuthentication (email: string, userID: string, token: string, expiresIn: number) {
