a94f574a1834afcb3357aabeb0f38429c17de538
PplPrinceDalsaniya <prince.dalsaniya@playpowerlabs.com>
2022-04-04 08:00:06 +0530

    Sending a SignUp request

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { AuthService } from "./auth.service";
 
 @Component ({
     selector: 'app-auth',
@@ -8,12 +9,31 @@ import { NgForm } from "@angular/forms";
 export class AuthComponent {
     isLoggedIn: boolean = false;
 
+    constructor ( private authService: AuthService){}
+
     onSwithMode() {
         this.isLoggedIn = !this.isLoggedIn;
     }
 
     onSubmit(form: NgForm) {
-        console.log(form.value);
+        if(!form.valid) return;
+
+        const email = form.value.email;
+        const password = form.value.password;
+        
+        if (this.isLoggedIn) {
+            
+        } else {
+            this.authService.signUp(email, password).subscribe(
+                resData => {
+                    console.log(resData);
+                },
+                error => {
+                    console.log(error);
+                }
+            );
+        }
+
         form.reset();
     }
 }
