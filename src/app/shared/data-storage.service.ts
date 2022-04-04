68c21e6eca3e17399291e60281adff3d68ee00a3
PplPrinceDalsaniya <prince.dalsaniya@playpowerlabs.com>
2022-04-04 11:04:36 +0530

    Attaching a token with Interceptor.

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -23,16 +23,9 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        return this.authService.user.pipe(
-            take(1),
-            exhaustMap(user => {
-                return this.http.get<Recipe[]>(
-                    "https://ng-recipebook-5e29c-default-rtdb.firebaseio.com/recipes.json",
-                    {
-                        params: new HttpParams().set('auth', user.token)
-                    }
-                );
-            }),
+        return this.http.get<Recipe[]>(
+            "https://ng-recipebook-5e29c-default-rtdb.firebaseio.com/recipes.json"
+        ).pipe(
             map(recipes => {
                 return recipes.map(recipe => {
                     return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] }
