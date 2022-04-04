8c152e0df71b914060d7de65aa9fc1c6c8f54940
PplPrinceDalsaniya <prince.dalsaniya@playpowerlabs.com>
2022-04-04 10:24:04 +0530

    Doing all changes related to AuthState in the UI

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { Router } from "@angular/router";
 import { Observable } from "rxjs";
 import { AuthResponseData, AuthService } from "./auth.service";
 
@@ -12,7 +13,10 @@ export class AuthComponent {
     isLoading: boolean = false;
     error: string = null;
 
-    constructor ( private authService: AuthService){}
+    constructor ( 
+        private authService: AuthService,
+        private router: Router
+    ){}
 
     onSwithMode() {
         this.isLoggedIn = !this.isLoggedIn;
@@ -37,6 +41,7 @@ export class AuthComponent {
             resData => {
                 console.log(resData);
                 this.isLoading = false;
+                this.router.navigate(['/recipes'])
             },
             errorMsg => {
                 console.log(errorMsg);
